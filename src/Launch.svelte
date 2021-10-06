dce073af908b88a38f554419bf7016a14e50c350
Thor Stenbæk <thorstenbaek@gmail.com>
2021-10-06 10:35:25 +0200

    Update Launch.svelte

--- src/Launch.svelte
+++ src/Launch.svelte
@@ -9,6 +9,8 @@
             redirectUri: './app',
             iss: 'https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4',
             launch:	'cdp2010025:encounter:resource'
+	    // iss=https://banan.dips.fhir.service.api.dips.no
+	    // launch=62247e85-c8c1-4047-90b3-e0b3a9c59600
         });
     })
 </script>
